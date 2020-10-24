aa31f2321a2fcb134a06ff919d3e1120f22f0749
João Pedro <54006291+Joao010@users.noreply.github.com>
2020-10-23 21:41:54 -0300

    Create 1082 - Componentes Conexos.cpp

---
+++ URI Solutions/1082 - Componentes Conexos.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;typedef long double lld;typedef string str;typedef pair<double, double> dd;
+typedef vector<int> vi;typedef vector<vector<int>> vvi;typedef vector<double> vd;typedef vector<ll> vll;
+typedef vector<lld> vld;typedef vector<char> vc;typedef vector<str> vs;typedef pair<int,int> ii;
+# define f(i,a,b,c) for(ll i=a;i<b;i+=c)
+# define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)
+# define w(x) while(x--)
+# define ctoi(a) (a-'0')
+# define pb push_back
+# define eb emplace_back
+# define lb lower_bound
+# define ub upper_bound
+# define len(x) x.length()
+# define be(x) x.begin(), x.end()
+# define rbe(x) x.rbegin(), x.rend()
+# define _(x) cout.precision(x);cout.setf(ios::fixed);
+# define ft first
+# define se second
+
+const int BR = 0, CI = 1, PR = 2;
+
+int t, n, m, ou;
+char au;
+vector<vc> adj;
+vi vis;
+vc res;
+
+void dfs(int u){
+    vis[u] = CI;
+    res.pb(char(u));
+    for (char v : adj[u]){
+        ou=int(v);
+        if (vis[ou]==BR){
+            dfs(ou);
+        }
+    }
+    vis[u] = PR;
+}
+
+char u, v;
+int pos, re;
+int main(){_(1)
+    cin>>t;
+    f(z,0,t,1){
+        re=0;
+        cin>>n>>m;
+        adj = vector<vc>(n+97);
+        f(i,97,n+97,1)adj[i].eb(char(97));
+        f(i,0,m,1){
+            cin>>u>>v;
+            ou=u;
+            if(i==0)pos=ou;
+            adj[ou].eb(v);
+            adj[int(v)].eb(u);
+        }
+        vis = vi(n+97, BR);
+        cout<<"Case #"<<z+1<<":\n";
+        f(i,97,n+97,1){
+            if(vis[i]==BR){
+                ++re;
+                res.clear();
+                dfs(i);
+                sort(be(res));
+                f(i,0,res.size(),1)cout<<res[i]<<',';
+                cout<<'\n';
+            }
+        }
+        cout<<re<<" connected components\n\n";
+        //if(z<t-1)cout<<'\n';
+    }
+return 0;}
