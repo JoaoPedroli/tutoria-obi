c2af729547d2308007e3627a146cba6e6b5a4ba4
João Pedro <54006291+Joao010@users.noreply.github.com>
2020-10-30 16:55:49 -0300

    Create 1583 - Contaminação.cpp

---
+++ URI/1583 - Contaminação.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;typedef long double lld;typedef string str;typedef pair<double, double> dd;
+typedef vector<int> vi;typedef vector<vector<int>> vvi;typedef vector<double> vd;typedef vector<ll> vll;
+typedef vector<lld> vld;typedef vector<char> vc;typedef vector<str> vs;typedef pair<int,int> ii;
+# define f(i,a,b,c) for(ll i=a;i<b;i+=c)
+# define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)
+# define w(x) while(x--)
+# define ctoi(a) (a-'0')
+# define pb push_back
+# define eb emplace_back
+# define lb lower_bound
+# define ub upper_bound
+# define len(x) x.length()
+# define be(x) x.begin(), x.end()
+# define rbe(x) x.rbegin(), x.rend()
+# define _(x) cout.precision(x);cout.setf(ios::fixed);
+# define ft first
+# define se second
+
+const int MAX = 51;
+vs ns;
+vvi memo;
+int n,m;
+
+void dfs(int i, int j){
+    if(i==n||i==-1||j==m||j==-1)return;
+    if(memo[i][j] or ns[i][j]=='X')return;
+
+    memo[i][j] = 1, ns[i][j] = 'T', dfs(i+1, j), dfs(i-1, j), dfs(i,j-1), dfs(i, j+1);
+}
+
+int main(){_(0)
+    ns = vs(MAX);
+    while(cin>>n>>m&&n||m){
+        memo = vvi(n+1, vi(m+1));
+        f(i,0,n,1){
+            cin>>ns[i];
+        }
+        f(i,0,n,1){
+            f(j,0,len(ns[i]),1){
+                if(ns[i][j]=='T' and !memo[i][j])dfs(i+1, j), dfs(i-1, j), dfs(i,j-1), dfs(i, j+1);
+            }
+        }
+        f(i,0,n,1){
+            for(char v:ns[i]){
+                cout<<v;
+            }
+            cout<<'\n';
+        }
+        cout<<'\n';
+    }
+
+return 0;}
+/*
+-preste atenção nos detalhes, veja se não repetiu nehuma variável ex.:(val[b]>val[b]) numa comparação, etc.
+*/
