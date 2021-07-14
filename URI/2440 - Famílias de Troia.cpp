e6eefe0a67752538edc93785c493e5ac8dc52dde
João Pedro <54006291+Joao010@users.noreply.github.com>
2021-07-14 10:58:27 -0300

    Update 2440 - Famílias de Troia.cpp

--- URI/2440 - Famílias de Troia.cpp
+++ URI/2440 - Famílias de Troia.cpp
@@ -1,47 +1,98 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;typedef long double lld;typedef string str;typedef pair<double, double> dd;
-typedef vector<int> vi;typedef vector<vector<int>> vvi;typedef vector<double> vd;typedef vector<ll> vll;
-typedef vector<lld> vld;typedef vector<char> vc;typedef vector<str> vs;typedef pair<int,int> ii;
-# define f(i,a,b,c) for(ll i=a;i<b;i+=c)
-# define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)
-# define w(x) while(x--)
-# define ctoi(a) (a-'0')
-# define pb push_back
-# define eb emplace_back
-# define lb lower_bound
-# define ub upper_bound
-# define len(x) x.length()
-# define be(x) x.begin(), x.end()
-# define rbe(x) x.rbegin(), x.rend()
-# define _(x) cout.precision(x);cout.setf(ios::fixed);
-# define ft first
-# define se second
+#define vt vector
+typedef pair<int, int> ii; typedef long long ll; typedef vt<int> vi;
+typedef vt<vt<int>> vvi; typedef vt<string> vs; typedef vt<ii> vii;
 
-vvi gr;
-vi memo;
-int n, m, a, b, re=0;
+#define _outputConfig(a) if(a) ios::sync_with_stdio(0);cin.tie(0)
+#define _fixAll(x) if(x >= 0) cout.precision(x);cout.setf(ios::fixed)
+#define eb emplace_back
+#define pb push_back
+#define f(a, b, c) for(int a = b; a < c; ++a)
+#define fd(a, b, c) for(int a = b; a >= c; --a)
+#define w(a) while(a--)
+#define s(a) int(a.size())
+#define l(a) a.length()
+#define be(ns) ns.begin(), ns.end()
+#define rbe(ns) ns.rbegin(), ns.rend()
+#define F first
+#define S second
+#define mdc(a, b) __gcd(a, b)
 
-void dfs(int i){
-    if(memo[i])return;
-    memo[i]=1;
-    for(int v:gr[i]){
-        dfs(v);
-    }
+const int MAX = 1e3 + 2, mVI = 1<<20, mVJ = 1<<10, mVZ = 105;
+const int INF = 0x3f3f3f3f, OUT = -INF, OUT2 = -1, MOD = 1e9 + 7;
+
+void log() { cout<<'\n'; }
+void log(int a) { cout<<a<<'\n'; }
+void log(vi a, int s = 0) {
+  cout<<a[s];
+  f(i, s+1, s(a))
+    cout<<' '<<a[i];
+  log();
 }
-int main(){_(1)
-    cin>>n>>m;
-    gr = vvi(n+1), memo = vi(n+1);
-    f(i,0,m,1){
-        cin>>a>>b;
-        gr[a].eb(b), gr[b].eb(a);
-    }
-    f(i,1,n+1,1){
-        if(!memo[i]){
-            dfs(i);
-            ++re;
-        }
-    }
-    cout<<re<<'\n';
+void log(vvi a, int s1 = 0, int s2 = 0) {
+  f(i, s1, s(a)) {
+    cout<<a[s1][s2];
+    f(j, s2+1, s(a[i]))
+      cout<<' '<<a[i][j];
+    log();
+  }
+}
+
+void CIN(vi &ns, int I, int s = 0) {
+  ns = vi(I);
+  f(i, s, I)
+    cin>>ns[i];
+}
+void CIN(vvi &ns, int I, int J, int s1 = 0, int s2 = 0) {
+  ns = vvi(I, vi(J));
+  f(i, s1, I)
+    f(j, s2, J)
+      cin>>ns[i][j];
+}
+
+int n, m, u, v, sum=0;
+vi vis;
+vvi ns;
+
+void dfs(int u) {
+  if(vis[u]) return;
+  vis[u] = 1;
+  for(int v:ns[u]) {
+    if(!vis[v]) dfs(v);
+  }
+}
+
+int main() {_outputConfig(0);_fixAll(0);
+
+  cin>>n>>m;
+  ns = vvi(n+1), vis = vi(n+1, 0);
+  f(i,0,m) {
+    cin>>u>>v;
+    ns[u].eb(v), ns[v].eb(u);
+  }
+  f(i,1,n+1)
+    if(!vis[i]) ++sum, dfs(i);
+  log(sum);
+
 return 0;}
+
+/* ALERTAS:
+
+  conflitos: [[
+    f(i,0,n) {
+      f(i,0,m) {
+      }
+    }
+
+    ns[i] > ns[i] /// ns1[i] > ns2[i]
+    ns[i] > ns[i] /// ns[i] > ns[j]
+  ]]
+
+  começos e limites de iterações: [[
+    f(i,a,b) /// f(i,a,b+1)
+    f(i,a,b) /// f(i,a-1,b)
+  ]]
+
+*/
