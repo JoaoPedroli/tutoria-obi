b1a8164409437dd7ebe6d8a215f6ca6049986fd9
João Pedro <54006291+JoaoPedroli@users.noreply.github.com>
2023-07-20 13:30:33 -0300

    Update 722C - Destroying Array.cpp

--- Codeforces/722C - Destroying Array.cpp
+++ Codeforces/722C - Destroying Array.cpp
@@ -1,64 +1,16 @@
 #include <bits/stdc++.h>
+#define int long long
+
 using namespace std;
 
-#define vt vector
-typedef pair<int, int> ii; typedef long long ll; typedef vt<int> vi;
-typedef vt<vi> vvi; typedef vt<string> vs; typedef vt<ii> vii;
+const int MAX = 1e5+5;
 
-#define _outputConfig(a) if(a) ios::sync_with_stdio(0);cin.tie(0)
-#define _fixAll(x) if(x >= 0) cout.precision(x);cout.setf(ios::fixed)
-#define eb emplace_back
-#define pb push_back
-#define f(a, b, c) for(int a = b; a < c; ++a)
-#define fd(a, b, c) for(int a = b; a >= c; --a)
-#define w(a) while(a--)
-#define s(a) int(a.size())
-#define l(a) a.length()
-#define be(ns) ns.begin(), ns.end()
-#define rbe(ns) ns.rbegin(), ns.rend()
-#define F first
-#define S second
-#define mdc(a, b) __gcd(a, b)
-
-const int MAX = 1e3 + 2, mVI = 1<<20, mVJ = 1<<10, mVZ = 105;
-const int INF = 0x3f3f3f3f, OUT = -INF, OUT2 = -1, MOD = 1e9 + 7;
-
-void log() { cout<<'\n'; }
-void log(int a) { cout<<a<<'\n'; }
-void log(vi a, int s = 0) {
-  cout<<a[s];
-  f(i, s+1, s(a))
-    cout<<' '<<a[i];
-  log();
-}
-void log(vvi a, int s1 = 0, int s2 = 0) {
-  f(i, s1, s(a)) {
-    cout<<a[s1][s2];
-    f(j, s2+1, s(a[i]))
-      cout<<' '<<a[i][j];
-    log();
-  }
-}
-
-void CIN(vi &ns, int I, int s = 0) {
-  ns = vi(I);
-  f(i, s, I)
-    cin>>ns[i];
-}
-void CIN(vvi &ns, int I, int J, int s1 = 0, int s2 = 0) {
-  ns = vvi(I, vi(J));
-  f(i, s1, I)
-    f(j, s2, J)
-      cin>>ns[i][j];
-}
-
-int n, ns[mVI], p[mVI];
-ll best, sum[mVI];
-vi in;
-vt<ll> out;
+int exist[MAX] = {0}, ns[MAX];
+int p[MAX], sum[MAX];
+vector<int> ans;
 
 int id(int u) {
-  return (p[u] == u) ? p[u] : p[u] = id(p[u]);
+  return (p[u] == u ? p[u] : p[u] = id(p[u]));
 }
 
 void join(int u, int v) {
@@ -66,43 +18,28 @@ void join(int u, int v) {
   p[u] = v, sum[v] += sum[u];
 }
 
-int main() {_outputConfig(0);_fixAll(0);
+signed main() {
+  ios_base::sync_with_stdio(0), cin.tie(nullptr);
 
-  cin>>n;
-  p[0] = -1, p[n+1] = -1;
-  f(i,1,n+1) {
+  int x, n; cin>>n;
+  for(int i = 1; i <= n; ++i) {
+    cin>>x;
+    p[i] = i, sum[i] = x;
+  }
+  for(int i = 1; i <= n; ++i) {
     cin>>ns[i];
-    p[i] = -1, sum[i] = ns[i];
   }
-  CIN(in, n);
-  fd(i,n-1,1) {
-    p[in[i]] = in[i];
 
-    if(p[in[i]-1] != -1) join(in[i], in[i]-1);
-    if(p[in[i]+1] != -1) join(in[i], in[i]+1);
-
-    best = max(best, sum[id(in[i])]), out.pb(best);
+  ans.emplace_back(0);
+  for(int i = n; i >= 1; --i) {
+    const int I = ns[i];
+    exist[I] = 1;
+    if(exist[I+1]) join(I, I+1);
+    if(exist[I-1]) join(I, I-1);
+    const int ma = max(ans.back(), sum[id(I)]);
+    ans.push_back(ma);
   }
-  fd(i,n-2,0) cout<<out[i]<<'\n';
-  log(0);
-
-return 0;}
-
-/* ALERTAS:
-
-  conflitos: [[
-    f(i,0,n) {
-      f(i,0,m) {
-      }
-    }
-
-    ns[i] > ns[i] /// ns1[i] > ns2[i]
-    ns[i] > ns[i] /// ns[i] > ns[j]
-  ]]
-
-  começos e limites de iterações: [[
-    f(i,a,b) /// f(i,a,b+1)
-    f(i,a,b) /// f(i,a-1,b)
-  ]]
-
-*/
+  for(int i = n-1; i >= 0; --i) {
+    cout<<ans[i]<<'\n';
+  }
+}
