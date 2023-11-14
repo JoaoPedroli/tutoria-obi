88271eb64d22989f686f4ca9df53d75b6d62d5ac
João Pedro <54006291+JoaoPedroli@users.noreply.github.com>
2023-11-14 18:56:34 -0300

    Update 10685 - Nature.cpp

--- UVa/10685 - Nature.cpp
+++ UVa/10685 - Nature.cpp
@@ -1,51 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ss = string;
 
-typedef long long ll;typedef long double lld;typedef string str;typedef pair<double, double> dd;
-typedef vector<int> vi;typedef vector<vector<int>> vvi;typedef vector<double> vd;typedef vector<ll> vll;
-typedef vector<lld> vld;typedef vector<char> vc;typedef vector<str> vs;typedef pair<int,int> ii;
-# define f(i,a,b,c) for(ll i=a;i<b;i+=c)
-# define fd(i,a,b,c) for(ll i=a;i>=b;i-=c)
-# define w(x) while(x--)
-# define ctoi(a) (a-'0')
-# define pb push_back
-# define eb emplace_back
-# define lb lower_bound
-# define ub upper_bound
-# define len(x) x.length()
-# define be(x) x.begin(), x.end()
-# define rbe(x) x.rbegin(), x.rend()
-# define _(x) cout.precision(x);cout.setf(ios::fixed);
-# define ft first
-# define se second
+struct DSU {
+	map<ss, ss> p;
+    map<ss, int> rnk, tam;
+	int ma;
 
-str ou, x, y;
-int n, m, q, ma;
-map<str, str> pai, ns;
-map<str, int> tam;
-
-str id(str u){
-    if(u==pai[u])return pai[u];
-    return pai[u] = id(pai[u]);
-}
-void join(str u, str v){
-    u = id(u), v = id(v);
-    if(u==v)return;
-    pai[v] = u, tam[u]+=tam[v];
-    ma = max(ma, tam[u]);
-}
-
-int main(){_(1)
-    while(cin>>n>>m&&n||m){
+	DSU() {
+        p.clear(), rnk.clear(), tam.clear();
         ma = 1;
-        f(i,0,n,1){
-            cin>>ou;
-            pai[ou] = ou, tam[ou] = 1;
-        }
-        f(i,0,m,1){
-            cin>>x>>y;
-            join(x, y);
-        }
-        cout<<ma<<'\n';
+	}
+
+	ss id(ss u) {
+		return (p[u] == u ? u : p[u] = id(p[u]));
+	}
+
+	bool same(ss u, ss v) {
+		return id(u) == id(v);
+	}
+	
+	void join(ss u, ss v) {
+		if(same(u, v)) return;
+		ss U = id(u), V = id(v);
+		if(rnk[U] < rnk[V]) swap(U, V);
+		p[U] = V;
+		if(rnk[U] == rnk[V]) ++rnk[V];
+		tam[V] += tam[U];
+        ma = max(ma, tam[V]);
+	}
+};
+
+int main() {
+  cin.tie(nullptr)->sync_with_stdio(0);
+
+  int n, m;
+  while(cin>>n>>m && n || m) {
+    ss s;
+    DSU dsu;
+    for(int i = 0; i < n; ++i) {
+      cin>>s;
+      dsu.p[s] = s;
+      dsu.tam[s] = 1;
+      dsu.rnk[s] = 0;
     }
-return 0;}
+    ss u, v;
+    for(int i = 0; i < m; ++i) {
+      cin>>u>>v;
+      dsu.join(u, v);
+    }
+    cout<<dsu.ma<<'\n';
+  }
+}
